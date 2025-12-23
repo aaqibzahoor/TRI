88f6742f1b42543d21770e0ef83a02945a36982f
hasaanqari9 <hassanqari9@gmail.com>
2025-12-23 08:43:12 +0530

    build error fix

--- src/app/home-3/page.tsx
+++ src/app/home-3/page.tsx
@@ -14,7 +14,7 @@ export default function HomeThree() {
     return (
         <>
             {/*-- Hero Area --*/}
-            <HeroSectionThree/>
+            <HeroSectionThree subtitle="" title="" description="" image=""/>
 
             {/*-- Service Section --*/}
             <ServiceSectionThree/>
